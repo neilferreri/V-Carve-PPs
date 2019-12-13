06557c70d447cd00627a13be50e6653296d69aea
neilferreri <neilferreri@gmail.com>
2019-12-12 20:11:38 -0500

    Add files via upload

---
+++ GRBL Toolchange/Shapeoko_inch_TC.pp
@@ -0,0 +1,159 @@
++================================================
++                                                
++ Grbl - Vectric machine output configuration file   
++                                                
++================================================
++                                                
++ History                                        
++                                                  
++ Who      When       What                         
++ ======== ========== ===========================
++ EdwardP  11/02/2015 Written from Grbl_mm.pp but
++                     set G20
++ EdwardP  11/02/2015 Commented out arcs as these
++                     slow GRBL performance appear 
++                     interpolated anyway
++ Mark     24/11/2015 Updated for interim 0.9 spec.
++                     Renaming to be machine specific.
++                     Removing M30 from Footer.
++ Ferreri  5/7/2018   Added G04 DWELL & M6 TOOLCHANGE
++================================================
+ 
+POST_NAME = "Shapeoko (inch) TOOLCHANGE(*.gcode)"
+ 
+FILE_EXTENSION = "gcode"
+ 
+UNITS = "INCHES"
+ 
++------------------------------------------------
++    Line terminating characters                 
++------------------------------------------------
+ 
+LINE_ENDING = "[13][10]"
+ 
++------------------------------------------------
++    Block numbering                             
++------------------------------------------------
+ 
+LINE_NUMBER_START     = 0
+LINE_NUMBER_INCREMENT = 10
+LINE_NUMBER_MAXIMUM = 999999
+ 
++================================================
++                                                
++    Formating for variables                     
++                                                
++================================================
+ 
+VAR LINE_NUMBER = [N|A|N|1.0]
+VAR SPINDLE_SPEED = [S|A|S|1.0]
+VAR FEED_RATE = [F|C|F|1.1]
+VAR X_POSITION = [X|C|X|1.4]
+VAR Y_POSITION = [Y|C|Y|1.4]
+VAR Z_POSITION = [Z|C|Z|1.4]
++VAR ARC_CENTRE_I_INC_POSITION = [I|A|I|1.4]
++VAR ARC_CENTRE_J_INC_POSITION = [J|A|J|1.4]
+VAR X_HOME_POSITION = [XH|A|X|1.4]
+VAR Y_HOME_POSITION = [YH|A|Y|1.4]
+VAR Z_HOME_POSITION = [ZH|A|Z|1.4]
+ 
++================================================
++                                                
++    Block definitions for toolpath output       
++                                                
++================================================
+ 
++---------------------------------------------------
++  Commands output at the start of the file
++---------------------------------------------------
+ 
+begin HEADER
+ 
+"T[T]"
+"G17"
+"G20"
+"G0[ZH]"
+"G0[XH][YH][S]M3"
+ 
+
++---------------------------------------------------
++  Commands output for tool change
++---------------------------------------------------
+ 
+begin TOOLCHANGE
+"M5"
+"M6 T[T]"
+"M3[S]"
+
++---------------------------------------------------
++  Commands output for rapid moves 
++---------------------------------------------------
+ 
+begin RAPID_MOVE
+ 
+"G0[X][Y][Z]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first feed rate move
++---------------------------------------------------
+ 
+begin FIRST_FEED_MOVE
+ 
+"G1[X][Y][Z][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for feed rate moves
++---------------------------------------------------
+ 
+begin FEED_MOVE
+ 
+"G1[X][Y][Z]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first clockwise arc move
++---------------------------------------------------
+
++begin FIRST_CW_ARC_MOVE
+
++"G2[X][Y][I][J][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for clockwise arc  move
++---------------------------------------------------
+ 
++begin CW_ARC_MOVE
+ 
++"G2[X][Y][I][J]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first counterclockwise arc move
++---------------------------------------------------
+ 
++begin FIRST_CCW_ARC_MOVE
+ 
++"G3[X][Y][I][J][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for counterclockwise arc  move
++---------------------------------------------------
+ 
++begin CCW_ARC_MOVE
+ 
++"G3[X][Y][I][J]"
+ 
+ 
++---------------------------------------------------
++  Commands output at the end of the file
++---------------------------------------------------
+ 
+begin FOOTER
+ 
+"G0[ZH]"
+"G0[XH][YH]"
+"M02"
