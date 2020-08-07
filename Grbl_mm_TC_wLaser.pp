011bbe6f85620387b88bea590dc6b72fb54b3c06
neilferreri <neilferreri@gmail.com>
2020-08-07 12:08:06 -0400

    Add files via upload

---
+++ Grbl_mm_TC_wLaser.pp
@@ -0,0 +1,241 @@
++================================================
++                                                
++ Grbl - Vectric machine output configuration file   
++                                                
++================================================
++                                                
++ History                                        
++                                                  
++ Who      When       What                         
++ ======== ========== ===========================
++ EdwardP  28/11/2014 Written from GCode_mm.pp
++                     Added arc move support  
++ EdwardP  11/02/2015 Commented out arcs as these
++                     slow GRBL performance appear 
++                     interpolated anyway
++ EdwardP  18/06/2015 Explicitly set absolute mode (G90)
++ Mark     24/11/2015 Updated for interim 0.9 spec.
++                     Renaming to be machine specific.
++                     Removing M30 from Footer.
++ EdwardP  01/02/2018 Updated according to 1.1 spec
++                     Changed to 1.3 precision
++                     Spindle off (M5) in footer
++                     Added VTransfer Direct Output
++ Ferreri  5/7/2018   Added G04 DWELL & M6 TOOLCHANGE
++ Ferreri  12/27/2019 Uncommented arc commands
++ Ferreri 1/25/2020   Ummmm...and related arc variables
++                     Added Rapid Plunge to StartZ
++ Ferreri	7/24/2020 Add Support for Laser Module
++ Ferreri	8/1/2020  Added workaround for Vectric Feedrate bug - Fixed in 10.506
++ ======== ========== ===========================
+
+POST_NAME = "Grbl (mm) TOOLCHANGE and LASER_SUPPORT (*.nc)"
+ 
+FILE_EXTENSION = "nc"
+ 
+UNITS = "MM"
+
+DIRECT_OUTPUT = "VTransfer"
+
+RAPID_PLUNGE_TO_STARTZ = "YES"
+LASER_SUPPORT = "YES"
+ 
++------------------------------------------------
++    Line terminating characters                 
++------------------------------------------------
+ 
+LINE_ENDING = "[13][10]"
+ 
++------------------------------------------------
++    Block numbering                             
++------------------------------------------------
+ 
+LINE_NUMBER_START     = 0
+LINE_NUMBER_INCREMENT = 10
+LINE_NUMBER_MAXIMUM = 999999
+ 
++================================================
++                                                
++    Formatting for variables                     
++                                                
++================================================
+ 
+VAR LINE_NUMBER = [N|A|N|1.0]
+VAR POWER = [P|C|S|1.0|10.0]
+VAR PLUNGE_RATE = [FP|C|F|1.0|1.0]							
+VAR SPINDLE_SPEED = [S|A|S|1.0]
+VAR FEED_RATE = [F|C|F|1.1]
+VAR X_POSITION = [X|C|X|1.4]
+VAR Y_POSITION = [Y|C|Y|1.4]
+VAR Z_POSITION = [Z|C|Z|1.4]
+VAR ARC_CENTRE_I_INC_POSITION = [I|A|I|1.4]
+VAR ARC_CENTRE_J_INC_POSITION = [J|A|J|1.4]
+VAR X_HOME_POSITION = [XH|A|X|1.3]
+VAR Y_HOME_POSITION = [YH|A|Y|1.3]
+VAR Z_HOME_POSITION = [ZH|A|Z|1.3]
+ 
++================================================
++                                                
++    Block definitions for toolpath output       
++                                                
++================================================
+ 
++---------------------------------------------------
++  Commands output at the start of the file
++---------------------------------------------------
+ 
+begin HEADER
+ 
+"G17"
+"G21"
+"G90"
+"T[T]"
+"[F]"
+"[S]M3"
+"G0[ZH]"
+"G0[XH][YH]"
+ 
++---------------------------------------------------
++  Commands output for tool change
++---------------------------------------------------
+ 
+begin TOOLCHANGE
+"M5"
+"M6 T[T]"
+"M3[S]"
+
++---------------------------------------------
++  Commands output for a dwell move
++---------------------------------------------
+
+begin DWELL_MOVE
+
+"G04 P[DWELL]"
+
+
++---------------------------------------------------
++  Command output after the header to switch spindle on
++---------------------------------------------------
+ 
+begin SPINDLE_ON
+
+"M3[S]"
+
+
++---------------------------------------------------
++  Commands output for rapid moves 
++---------------------------------------------------
+ 
+begin RAPID_MOVE
+ 
+"G0[X][Y][Z]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first feed rate move
++---------------------------------------------------
+ 
+begin FIRST_FEED_MOVE
+ 
+"G1[X][Y][Z][P][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for feed rate moves
++---------------------------------------------------
+ 
+begin FEED_MOVE
+ 
+"G1[X][Y][Z][P]"
+ 
++---------------------------------------------------
+
++ Commands output for the First Plunge Move, in a series of plunge moves.
+
++---------------------------------------------------
+
+begin FIRST_PLUNGE_MOVE
+"G1[Z][F]"
+
+
++---------------------------------------------------
++  Commands output for the plunge move
++---------------------------------------------------
+
+begin PLUNGE_MOVE
+
+"G1[Z]"
+
+
++---------------------------------------------------
++  Commands output for the first clockwise arc move
++---------------------------------------------------
+
+begin FIRST_CW_ARC_MOVE
+
+"G2[X][Y][I][J][F][P]"
+ 
+ 
++---------------------------------------------------
++  Commands output for clockwise arc  move
++---------------------------------------------------
+ 
+begin CW_ARC_MOVE
+ 
+"G2[X][Y][I][J]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first counterclockwise arc move
++---------------------------------------------------
+ 
+begin FIRST_CCW_ARC_MOVE
+ 
+"G3[X][Y][I][J][F][P]"
+ 
+ 
++---------------------------------------------------
++  Commands output for counterclockwise arc  move
++---------------------------------------------------
+ 
+begin CCW_ARC_MOVE
+ 
+"G3[X][Y][I][J]"
+ 
++---------------------------------------------------
++  Commands output when the jet is turned on
++---------------------------------------------------
+
+begin JET_TOOL_ON
+
+"M4[P]"
+
+ 
+ 
++---------------------------------------------------
++  Commands output when the jet is turned off
++---------------------------------------------------
+
+begin JET_TOOL_OFF
+
+"M5"
+
+ 
++---------------------------------------------------
++  Commands output when the jet power is changed
++---------------------------------------------------
+
+begin JET_TOOL_POWER
+
+"[P]"
+ 
++---------------------------------------------------
++  Commands output at the end of the file
++---------------------------------------------------
+ 
+begin FOOTER
+
+"M5"
+"G0[ZH]"
+"G0[XH][YH]"
+"M2"
