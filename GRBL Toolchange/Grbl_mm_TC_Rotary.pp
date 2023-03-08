62fa0394cd3be88d4484595d926f5c8908006581
neilferreri <neilferreri@gmail.com>
2023-03-08 07:57:34 -0500

    Add files via upload
    Grbl rotary support

---
+++ GRBL Toolchange/Grbl_mm_TC_Rotary.pp
@@ -0,0 +1,185 @@
++================================================
++                                                
++ Grbl - Vectric machine output configuration file   
++                                                
++================================================
++                                                
++ History                                        
++                                                  
++ Who      When       What                         
++ ======== ========== ===========================
++ EdwardP  28/11/2014 Written from GCode_mm.pp
++                     Added arc move support  
++ EdwardP  11/02/2015 Commented out arcs as these
++                     slow GRBL performance appear 
++                     interpolated anyway
++ EdwardP  18/06/2015 Explicitly set absolute mode (G90)
++ Mark     24/11/2015 Updated for interim 0.9 spec.
++                     Renaming to be machine specific.
++                     Removing M30 from Footer.
++ EdwardP  01/02/2018 Updated according to 1.1 spec
++                     Changed to 1.3 precision
++                     Spindle off (M5) in footer
++                     Added VTransfer Direct Output
++ Ferreri  5/7/2018   Added G04 DWELL & M6 TOOLCHANGE
++ Ferreri  12/27/2019 Uncommented arc commands
++ Ferreri 1/25/2020   Ummmm...and related arc variables
++                     Added Rapid Plunge to StartZ
++ Ferreri 3/8/2023	  Added Y Rotary (Wrap) Support
++
++ ======== ========== ===========================
+
+POST_NAME = "Grbl (mm) TC WRAP (*.nc)"
+ 
+FILE_EXTENSION = "nc"
+ 
+UNITS = "MM"
+
+DIRECT_OUTPUT = "VTransfer"
+
+RAPID_PLUNGE_TO_STARTZ = "YES"
+
+ROTARY_WRAP_Y = "Y"
+ 
++------------------------------------------------
++    Line terminating characters                 
++------------------------------------------------
+ 
+LINE_ENDING = "[13][10]"
+ 
++------------------------------------------------
++    Block numbering                             
++------------------------------------------------
+ 
+LINE_NUMBER_START     = 0
+LINE_NUMBER_INCREMENT = 10
+LINE_NUMBER_MAXIMUM = 999999
+ 
++================================================
++                                                
++    Formatting for variables                     
++                                                
++================================================
+ 
+VAR LINE_NUMBER = [N|A|N|1.0]
+VAR SPINDLE_SPEED = [S|A|S|1.0]
+VAR FEED_RATE = [F|C|F|1.1]
+VAR X_POSITION = [X|C|X|1.4]
+VAR Y_POSITION = [Y|C|Y|1.4]
+VAR Z_POSITION = [Z|C|Z|1.4]
+VAR ARC_CENTRE_I_INC_POSITION = [I|A|I|1.4]
+VAR ARC_CENTRE_J_INC_POSITION = [J|A|J|1.4]
+VAR X_HOME_POSITION = [XH|A|X|1.3]
+VAR Y_HOME_POSITION = [YH|A|Y|1.3]
+VAR Z_HOME_POSITION = [ZH|A|Z|1.3]
+ 
++================================================
++                                                
++    Block definitions for toolpath output       
++                                                
++================================================
+ 
++---------------------------------------------------
++  Commands output at the start of the file
++---------------------------------------------------
+ 
+begin HEADER
+ 
+"G17"
+"G21"
+"G90"
+"T[T]"
+"[S]M3"
+"G0[ZH]"
+"G0[XH][YH]"
+ 
++---------------------------------------------------
++  Commands output for tool change
++---------------------------------------------------
+ 
+begin TOOLCHANGE
+"M5"
+"M6 T[T]"
+"M3[S]"
+
++---------------------------------------------
++  Commands output for a dwell move
++---------------------------------------------
+
+begin DWELL_MOVE
+
+"G04 P[DWELL]"
+ 
++---------------------------------------------------
++  Commands output for rapid moves 
++---------------------------------------------------
+ 
+begin RAPID_MOVE
+ 
+"G0[X][Y][Z]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first feed rate move
++---------------------------------------------------
+ 
+begin FIRST_FEED_MOVE
+ 
+"G1[X][Y][Z][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for feed rate moves
++---------------------------------------------------
+ 
+begin FEED_MOVE
+ 
+"G1[X][Y][Z]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first clockwise arc move
++---------------------------------------------------
+
+begin FIRST_CW_ARC_MOVE
+
+"G2[X][Y][I][J][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for clockwise arc  move
++---------------------------------------------------
+ 
+begin CW_ARC_MOVE
+ 
+"G2[X][Y][I][J]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first counterclockwise arc move
++---------------------------------------------------
+ 
+begin FIRST_CCW_ARC_MOVE
+ 
+"G3[X][Y][I][J][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for counterclockwise arc  move
++---------------------------------------------------
+ 
+begin CCW_ARC_MOVE
+ 
+"G3[X][Y][I][J]"
+ 
+ 
++---------------------------------------------------
++  Commands output at the end of the file
++---------------------------------------------------
+ 
+begin FOOTER
+
+"M5"
+"G0[ZH]"
+"G0[XH][YH]"
+"M2"
